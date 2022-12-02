4966483438f8b6eb00bdc8c3dcd2a8a7ef576a49
Shreyam Pokharel <pshreyam@gmail.com>
2022-12-02 14:25:02 +0545

    Display first and follow in html table

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, redirect, url_for, request
 # from parse_table_generator import table
 from left_recursion import left_recursion
 from left_factoring import left_factor
+from first_n_follow import get_first, get_follow, prepare_grammar
 
 app = Flask(__name__)
 
@@ -11,9 +12,27 @@ app = Flask(__name__)
 def index():
     if request.method == "POST":
         grammar = request.form.get("grammar")
-        left_recursion_free_grammar = left_recursion(grammar[:])
-        left_factored_grammar = left_factor(left_recursion_free_grammar[:])
-        return render_template("index.html", 
-            left_recursion_free_grammar=left_recursion_free_grammar, 
-            left_factored_grammar=left_factored_grammar)
+        if grammar.strip():
+            left_recursion_free_grammar = left_recursion(grammar)
+            left_factored_grammar = left_factor(left_recursion_free_grammar[:])
+            a, b, c, non_terminals, terminals = prepare_grammar(left_factored_grammar)
+
+            first = {}
+            follow = {f"{non_terminals[0]}" : {'$'}}
+
+            for each_non_terminal in non_terminals:
+                get_first(each_non_terminal, first, c, terminals, non_terminals)
+
+            get_follow(follow, c, terminals, non_terminals, first)
+
+            return render_template("index.html",
+                left_recursion_free_grammar=left_recursion_free_grammar,
+                left_factored_grammar=left_factored_grammar,
+                grammar_body=grammar.strip(),
+                first=first,
+                follow=follow,
+                terminals=terminals,
+                non_terminals=non_terminals)
+        else:
+            return redirect(url_for("index"))
     return render_template("index.html")
--- first_n_follow.py
+++ first_n_follow.py
@@ -1,31 +1,9 @@
 import re
 from prettytable import PrettyTable
-grammars  = ['''S -> A
-A -> a A''
-A' -> d A' | e A' | ε
-A'' -> B A' | C A'
-B -> b B c | f
-C -> g'''
-, '''S -> A a A b | B b B a
-A -> ε
-B -> ε'''
-,'''S -> A B C
-A -> D E F
-B -> ε
-C -> ε
-D -> ε
-E -> ε
-F -> ε''',
-'''E -> T E'
-E' -> + T E' | ε
-T -> F T'
-T' -> * F T' | ε
-F -> ( E ) | id
-''']
 
-for grammar in grammars:
-    grammar = grammar.strip()
-    a = re.split("\n", grammar)
+
+def prepare_grammar(grammar):
+    a = grammar[:]
     b = []
     c = []
     every_variable = []
@@ -33,7 +11,7 @@ for grammar in grammars:
 
     for element in a:
         b.append(re.split(r"->|\|", element))
-        
+
     for element in b:
         d = []
         for side in element:
@@ -42,63 +20,77 @@ for grammar in grammars:
         c.append(d)
 
     for element in c:
-        joined = (' | '.join(str(a) for a in element[1:]))
         non_terminal.append(*element[0].split())
         for each in element:
             every_variable.extend(each.split())
-        
-        
+
     terminal = set(every_variable) - set(non_terminal)
 
-    print(f'The given grammar is:\n{grammar}')
-    def get_first(a, first, c):
-        if first.get(a):
-            return first[a]
-        elif a in terminal:
-            return {a}
-        else:
-            first[a] = set([])
-            for element in c:
-                if element[0] == a:
-                    for each in (element[1:]):
-                        value = 0
-                        while ('ε' in get_first(each.split()[value], first, c) and len(each.split()[value+1:]) > 0):
-                            first[a].update(get_first(each.split()[value], first, c) - {'ε'})
-                            value = value +1 
-                        first[a].update(get_first(each.split()[value], first, c))
-            return first[a]
+    return a, b, c, non_terminal, terminal
 
-    def get_follow( follow, c):
+
+def get_first(a, first, c, terminal, non_terminal):
+    if first.get(a):
+        return first[a]
+    elif a in terminal:
+        return {a}
+    else:
+        first[a] = set([])
         for element in c:
-            for each in element[1:]:
-                j = each.split()
-                for i in range(len(j)):
-                    
-                    if j[i] in non_terminal:
-                        if not follow.get(j[i]):
-                            follow[j[i]]= set([])
-                        if i < len(j)-1:
-                            if j[i+1] in non_terminal:
-                                value = i+1
-                                while ('ε' in first[j[value]] and len(j[value+1:]) >= 1):
-                                    follow[j[i]].update(first[j[value]] - {'ε'})
-                                    value = value+1
+            if element[0] == a:
+                for each in (element[1:]):
+                    value = 0
+                    while ('ε' in get_first(each.split()[value], first, c, terminal, non_terminal) and len(each.split()[value+1:]) > 0):
+                        first[a].update(get_first(each.split()[value], first, c, terminal, non_terminal) - {'ε'})
+                        value = value + 1
+                    first[a].update(get_first(each.split()[value], first, c, terminal, non_terminal))
+        return first[a]
+
+
+def get_follow(follow, c, terminal, non_terminal, first):
+    for element in c:
+        for each in element[1:]:
+            j = each.split()
+            for i in range(len(j)):
+
+                if j[i] in non_terminal:
+                    if not follow.get(j[i]):
+                        follow[j[i]]= set([])
+                    if i < len(j)-1:
+                        if j[i+1] in non_terminal:
+                            value = i + 1
+                            while ('ε' in first[j[value]] and len(j[value+1:]) >= 1):
                                 follow[j[i]].update(first[j[value]] - {'ε'})
-                                if len(j[value+1:]) <= 0:
-                                    follow[j[i]].update(follow[element[0]])
-                            else:
-                                follow[j[i]].update({j[i+1]})
-                        else:               
-                            follow[j[i]].update(follow[element[0]])
-            
+                                value = value + 1
+                            follow[j[i]].update(first[j[value]] - {'ε'})
+                            if len(j[value+1:]) <= 0:
+                                follow[j[i]].update(follow[element[0]])
+                        else:
+                            follow[j[i]].update({j[i+1]})
+                    else:
+                        follow[j[i]].update(follow[element[0]])
+
+
+if __name__ == "__main__":
+    grammar = [
+        "S -> A B C",
+        "B -> b B'",
+        "B' -> C B' | ε",
+        "A -> a b A'",
+        "A' -> A | ε",
+        "C -> c C'",
+        "C' -> ε | C"
+    ]
+
+    a, b, c, non_terminal, terminal = prepare_grammar(grammar)
     first = {}
     follow = {f"{non_terminal[0]}" : {'$'}}
 
     for each_non_terminal in non_terminal:
-        get_first(each_non_terminal, first, c)
+        get_first(each_non_terminal, first, c, terminal, non_terminal)
 
-    get_follow(follow,c)
-    
+    get_follow(follow, c, terminal, non_terminal, first)
+
     x = PrettyTable()
     x.field_names = ['Non Terminals', 'First', 'Follow']
     for each in non_terminal:
