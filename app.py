d9477a8ed7cab913ba4ac3e979fc53123f74058e
Shreyam Pokharel <pshreyam@gmail.com>
2022-12-02 11:37:00 +0545

    Add files

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
+
+from parse_table_generator import table
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Compiler Design Miniproject"
+    return render_template("index.html", table=table)
---
+++ parse_table_generator.py
@@ -0,0 +1,6 @@
+first_set = {'S': ''}
+follow_set = {'S': {''}}
+
+table = [[1, 2, 3,], [1, 2, 3,], [1, 2, 3,],]
+
+print(table)
